04b550bf437cf2ae6a61b68a6a62073c1790e4c2
Andrew <53382306+Dawnvibe@users.noreply.github.com>
2021-08-29 18:33:09 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,24 @@
+/*
+const openForm = document.getElementById('openForm');
+const myForm = document.getElementById('myForm');
+const closeForm = document.getElementById('closeForm');
+
+/*
+openForm.addEventListener('click', () => {
+  myForm.classList.add('show');
+});
+
+closeForm.addEventListener('click', () => {
+  myForm.classList.remove('show');
+});
+
+let openForm*/
+
+/*
+var x = document.getElementById("myForm");
+if (x.style.display === "none") {
+  x.style.display = "flex";
+} else {
+  x.style.display = "none";
+}
+*/
